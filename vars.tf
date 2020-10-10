c78c46aeeecd8bf1459dd5a85f64d05f27ab66f6
Ajay Kumar <ajay011.sharma@hotmail.com>
2020-10-10 17:20:28 +0530

    update

---
+++ vars.tf
@@ -0,0 +1,13 @@
+variable "AWS_REGION" {
+  default = "ap-south-1"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-107d3e61"
+    ap-south-1 = "ami-02b5fbc2cb28b77b8"
+    eu-west-1 = "ami-044f36cc778038e81"
+  }
+}
+
